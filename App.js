de204ca87284ba5ba57a8baf0d4da8d02e45a07a
marcelo <mark.gt.mf@gmail.com>
2021-08-28 23:02:43 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View,Text,Button, ScrollView,ImageBackground,StyleSheet, TouchableOpacity} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as firebase from 'firebase';
 
-export default function App() {
+  function HomeScreen ({navigation}) {
+    return(
+      <View style= {{flex:1}}>
+
+        <ScrollView horizontal contentContainerStyle={{width:'200%',height:250}} style= {{flex:1}}>
+
+          <ImageBackground style={styles.image} source={{ uri: 'https://pdfmaranhao.com.br/storage/uploads/images/empresas/perfil/NVFRyxkmIL0J0OMOHfUfQWWJHubRNNlr2M3GCBr5.png' }}>
+              <TouchableOpacity onPress={()=> navigation.navigate('Noticia')} style={{
+                width:'100%',
+                height:'100%',
+                backgroundColor:'rgba(0,0,0,0.5)',
+                justifyContent:'flex-end'
+              }}>
+                <Text style={{fontSize:27,color:'white'}}>Noticia de exemplo</Text>
+              </TouchableOpacity>
+          </ImageBackground>
+
+          <ImageBackground style={styles.image} source={{ uri: 'https://pdfmaranhao.com.br/storage/uploads/images/empresas/perfil/NVFRyxkmIL0J0OMOHfUfQWWJHubRNNlr2M3GCBr5.png' }}>
+              <TouchableOpacity style={{
+                width:'100%',
+                height:'100%',
+                backgroundColor:'rgba(0,0,0,0.5)',
+                justifyContent:'flex-end'
+              }}>
+                <Text style={{fontSize:27,color:'white'}}>Noticia de exemplo</Text>
+              </TouchableOpacity>
+          </ImageBackground>
+
+
+        </ScrollView>
+
+
+      </View>
+    );
+  }
+  function NoticiaScreen ({navigation}) {
+    return(
+      <View style= {{flex:1, alignItems:'center',justifyContent:'center'}}>
+        
+      </View>
+    );
+  }
+
+  const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Portal" component={HomeScreen} />
+        <Stack.Screen name="Noticia" component={NoticiaScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  image: {
+    flex:1,
+    resizeMode: 'cover',
+    justifyContent:'center',
+    width:'100%'
+  }
 });
+
+export default App;
